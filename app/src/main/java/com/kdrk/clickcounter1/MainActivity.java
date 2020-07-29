f726acc95e10b3f4809e2665f0f4ca0bf3c5e72f
Konstantin Chernov <kdrk.soft@gmail.com>
2020-07-29 16:06:42 +0300

    счетчик

--- app/src/main/java/com/kdrk/clickcounter1/MainActivity.java
+++ app/src/main/java/com/kdrk/clickcounter1/MainActivity.java
@@ -6,18 +6,26 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvResult;
     public int counter = 0;
-    SharedPreferences sharedPreferences;
 
+    SharedPreferences sharedPreferences;
+    private Animation anim;
+    private ImageView viewAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,46 +35,52 @@ public class MainActivity extends AppCompatActivity {
         LoadInt();
         tvResult.setText(Integer.toString(counter));
 
-
     }
     public void SaveInt(String key, int counter){
 
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putInt(key, counter); editor.commit();
+        editor.putInt(key, counter);
+        editor.commit();
     }
 
     public void LoadInt() {
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         counter = sharedPreferences.getInt("key", counter);
-    }
 
+    }
 
     public void init(){
         tvResult = findViewById(R.id.tvResult);
-
+        viewAnim = findViewById(R.id.imageView);
+        anim = AnimationUtils.loadAnimation(this, R.anim.mytrans);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         Intent i = new Intent(MainActivity.this, MainMenu.class);
-
         i.putExtra("count", counter+"");
         startActivity(i);
-
         return super.onOptionsItemSelected(item);
     }
 
     public void onClickCount(View view) {
+
         counter++;
         tvResult.setText(Integer.toString(counter));
+        Toast toast = Toast.makeText(getApplicationContext(), "ещё", Toast.LENGTH_SHORT);
+        //toast.setGravity(Gravity.CENTER, 0,400);
+        toast.show();
+        viewAnim.startAnimation(anim);
+
+
 
 
     }
--- app/src/main/java/com/kdrk/clickcounter1/MainMenu.java
+++ app/src/main/java/com/kdrk/clickcounter1/MainMenu.java
@@ -2,12 +2,17 @@ package com.kdrk.clickcounter1;
 
 import android.app.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainMenu extends Activity {
     private TextView  tvLarge;
     String count;
+    private Button reset;
+    int a;
 
 
 
@@ -16,10 +21,15 @@ public class MainMenu extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         tvLarge = findViewById(R.id.tvLarge);
+        reset = findViewById(R.id.reset);
         count = getIntent().getStringExtra("count");
-        tvLarge.setText(count.toString());
+        tvLarge.setText(count);
 
     }
 
+    public void onClickReset(View view) {
+        tvLarge.setText("0");
+
+    }
 
 }
